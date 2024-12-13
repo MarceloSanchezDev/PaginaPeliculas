efe7d27ea2e52ffb590703b39ae3a8f5df3eaa77
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2024-12-13 11:14:23 -0300

    implementacion de componentes Footer, Header

--- appPeliculas/src/App.jsx
+++ appPeliculas/src/App.jsx
@@ -3,17 +3,21 @@ import {Routes, Route} from 'react-router-dom'
 //Componentes
 import Login from "./components/Login"
 import Listado from './components/Listado'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
+      <Header/>
     {/*Routes es lo mismo que Switch */}
       <Routes>
         {/*Route es la ruta donde se renderiza el componente */}
         <Route exact path='/' Component={Login}/>
         <Route path='/listado' Component={Listado}/>
       </Routes>
+      <Footer/>
     </>
   )
 }
---
+++ appPeliculas/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+    return(
+        <footer>
+            <nav>
+                <ul>
+                    <li><a href="https://www.instagram.com">Instagram</a></li>
+
+                </ul>
+            </nav>
+            <p>Copytight Marcelo Sanchez</p>
+        </footer>
+    )
+
+}
+
+export default Footer
