824ad2a34418915ea033a25c1d15e08cc96b663e
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2024-12-19 14:46:50 -0300

    fix back

--- backend/routes/auth.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { UserController } from '../controllers/user.js'
-
-export const authRouter = Router()
-
-authRouter.post('/login', UserController.login)
-
-authRouter.post('/register', UserController.registerUser)
