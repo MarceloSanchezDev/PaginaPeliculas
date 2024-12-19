247e34a387423e196551e29d9d9e04f7a97294fd
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2024-12-19 14:42:22 -0300

    agregamos el back

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,7 @@
-import express, {json} from 'express'
-import cors from 'cors'
+import { app } from './app.js'
+import { PORT } from './utils/config.js'
+import { info } from './utils/logger.js'
 
-const app = express()
-app.use(json())
-app.use(cors())
-const PORT = 3000
-
-app.get('/',(req,res)=>{
-    res.json({user: 'Marcelo'})
-})
-app.post('/login',(req,res)=>{
-    console.log(req.body)
-    res.status(200).json(req.body)
-})
-
-app.listen(PORT,()=>{
-    console.log(`escuchando desde http://localhost:${PORT}`)
+app.listen(PORT, () => {
+  info(`listening on port http://localhost:${PORT}`)
 })
